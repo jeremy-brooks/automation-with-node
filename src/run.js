1d9b1a99dcaa14a01b0d7faa5a71760e8b60b5e0
jeremy-brooks <jeremy.brooks@mapofmedicine.com>
2016-07-25 15:42:12 +0100

    Simplified code and tests

--- src/run.js
+++ src/run.js
@@ -17,19 +17,6 @@ exports.setRootContext = function (value) {
     }
 };
 
-/*Pre-deployment steps
- Take a backup of each servlet container (e.g. Apache Tomcat)
- Stop application servers individually making use of the load balancer to ensure minimal customer impact
- /*
- Stop tomcat.
- /etc/init.d/tomcat{#} stop
- Check status of tomcat to make sure it has stopped.
- /etc/init.d/tomcat{#} status
- If each tomcat instance has stopped successfully then move onto step 2, otherwise you may have to kill the tomcat process by id.
- /etc/init.d/tomcat{#} killtomcat
- */
-
-
 /*
  Take backup of all servlet containers state before release (e.g /{tomcat home}).
  sudo mkdir -p /var/tmp/tomcatReleaseBackup_{YYYYMMDDTHH-MM-SS}
@@ -47,13 +34,6 @@ exports.backupTomcatByServerNumber = function (serverNumber, callBack) {
     var destination = tomcatBackupLocation + "/server" + serverNumber;
     copy(source, destination, callBack);
 };
-
-/*
- Start all servlet containers (e.g. Apache Tomcat).
- /etc/init.d/tomcat{#} start
- Check that each servlet container has started
- /etc/init.d/tomcat{#} status
- */
 
 /*Deployment steps*/
 
