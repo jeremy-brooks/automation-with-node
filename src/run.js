bdaf540f86dba7a7414c14d73d395a57e3ae2738
jeremy-brooks <jeremy.brooks@mapofmedicine.com>
2016-07-25 16:00:32 +0100

    Added tests for extra step and properly handled errors

--- src/run.js
+++ src/run.js
@@ -6,11 +6,12 @@ var copy = require("ncp").ncp;
 
 var releaseBackupFolderNamePlaceholder = "{folderName}";
 var releaseBackupLocation = "var/tmp/tomcatReleaseBackup_" + releaseBackupFolderNamePlaceholder;
+var tomcatReleaseLocation = "var/tmp/tomcatRelease_" + releaseBackupFolderNamePlaceholder;
 var tomcatBackupLocation = null;
 var tomcatLocation = "var/tomcat";
 var rootContext = "/";
 
-exports.dateTimeFormat = "YYYYMMDDThh-mm-ss";
+exports.dateTimeFormat = "YYYYMMDDThh-mm-ss_SSS";
 exports.setRootContext = function (value) {
     if (typeof value === "string" && value.length > 0){
         rootContext = value;
@@ -33,6 +34,17 @@ exports.backupTomcatByServerNumber = function (serverNumber, callBack) {
     var source = rootContext + tomcatLocation + "/server" + serverNumber;
     var destination = tomcatBackupLocation + "/server" + serverNumber;
     copy(source, destination, callBack);
+};
+
+/*
+Prepare Apache Tomcat with the new release(s) so deploying them later is just a matter of copying the entire folder and starting Tomcat.
+    Create a copy of the backup so you can prepare the new release for later
+    sudo mkdir -p /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}
+    sudo cp -rp /var/tmp/tomcatReleaseBackup_{YYYYMMDDTHH-MM-SS}/* /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/
+*/
+exports.createTomcatReleaseFolder = function (folderName, callBack) {
+    tomcatReleaseLocation = rootContext + tomcatReleaseLocation.replace(releaseBackupFolderNamePlaceholder, folderName);
+    fileSystem.mkdir(tomcatReleaseLocation, callBack);
 };
 
 /*Deployment steps*/
--- test/test.js
+++ test/test.js
@@ -15,19 +15,44 @@ describe("Run", function() {
         run.setRootContext(rootContextForTests);
     });
 
-    describe("createTomcatBackupFolder()", function () {
-        it("should create a folder without error", function (done) {
-            try {
-                run.createTomcatBackupFolder(releaseBackupFolderName, function () {
-                    run.backupTomcatByServerNumber("1", function () {
-                        run.backupTomcatByServerNumber("2", function () {
-                            done();
+    describe("Take a backup of each servlet container", function () {
+        it("should backup each erver without error", function (done) {
+            run.createTomcatBackupFolder(releaseBackupFolderName, function (error) {
+                if (error) throw error;
+                run.backupTomcatByServerNumber("1", function (error) {
+                    if (error) throw error;
+                    run.backupTomcatByServerNumber("2", function (error) {
+                        if (error) throw error;
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    describe("Prepare Apache Tomcat with the new release(s) so deploying them later is just a matter of copying the entire folder and starting Tomcat", function () {
+
+        before(function () {
+            releaseBackupFolderName = moment().format(run.dateTimeFormat);
+        });
+
+        describe("Create a copy of the backup so you can prepare the new release for later", function () {
+            it("completes without error", function (done) {
+                run.createTomcatBackupFolder(releaseBackupFolderName, function (error) {
+                    if (error) throw error;
+                    run.backupTomcatByServerNumber("1", function (error) {
+                        if (error) throw error;
+                        run.backupTomcatByServerNumber("2", function (error) {
+                            if (error) throw error;
+                            run.createTomcatReleaseFolder(releaseBackupFolderName, function (error) {
+                                if (error) throw error;
+                                done();
+                            })
                         });
                     });
                 });
-            } catch (error){
-                throw error;
-            }
-        });
+            });
+        })
+
     });
 });
