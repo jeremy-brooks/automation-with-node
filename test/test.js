60dcb46c885810f1d61207e1744f97408f64d8ef
jeremy-brooks <jeremy.brooks@mapofmedicine.com>
2016-07-25 12:08:49 +0100

    changed descriptions a little

--- test/test.js
+++ test/test.js
@@ -8,13 +8,13 @@ var moment = require("moment");
 var releaseBackupFolderName = "";
 
 describe("Run", function() {
-    describe("prepReleaseBackupFolder", function () {
+    describe("prepReleaseBackupFolder()", function () {
 
         before(function () {
             releaseBackupFolderName = moment().format(run.dateTimeFormat);
         });
 
-        it("Creates a backup folder in /var/tmp with date time as name", function (done) {
+        it("should create a folder without error", function (done) {
             run.prepReleaseBackupFolder(releaseBackupFolderName, function (error) {
                 if (error) {
                     throw error;
