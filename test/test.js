49bafe87a183caf0e90f4258d0885bf0d53cf37d
jeremy-brooks <jeremy.brooks@mapofmedicine.com>
2016-07-26 17:07:29 +0100

    completed reverted all code back to basics

--- test/test.js
+++ test/test.js
@@ -1,74 +1,148 @@
-/**
- * Created by jbrooks on 25/07/2016.
- */
-var run = require("../src/automate");
+var assert = require("chai").assert;
+var fileSystem = require("fs-extra");
 var moment = require("moment");
 var path = require("path");
 var rimraf = require("rimraf");
 
-var releaseBackupFolderName = "";
-var rootContextForTests = "area/";
+describe("Pre-deployment steps", function () {
 
-function clearUpTestDirectory(directoryToDelete, callback){
-    directoryToDelete = path.normalize(directoryToDelete);
-    directoryToDelete = path.resolve(directoryToDelete);
-    rimraf(directoryToDelete, callback);
-}
-
-describe("Run", function() {
+    var timestamp = "";
+    var dateTimeFormat = "YYYYMMDDThh-mm-ss_SSS";
+    var timestampPlaceHolder = "{timestamp}";
+    var preDeploymentBaseDirectory = "sandbox/pre-deployment";
+    var tomcatReleaseBackupUri = preDeploymentBaseDirectory + "/var/tmp/" + timestampPlaceHolder + "_tomcatReleaseBackup";
 
     before(function (done) {
-        clearUpTestDirectory(rootContextForTests + "var/tmp/*", function (error) {
+        timestamp = moment().format(dateTimeFormat);
+        fileSystem.mkdirs(preDeploymentBaseDirectory, function (error) {
             if (error) throw error;
-            releaseBackupFolderName = moment().format(run.dateTimeFormat);
-            run.setRootContext(rootContextForTests);
             done();
         });
     });
 
     after(function (done) {
-        clearUpTestDirectory(rootContextForTests + "var/tmp/*", function (error) {
+        var directoryToDelete = path.normalize(preDeploymentBaseDirectory);
+        directoryToDelete = path.resolve(directoryToDelete);
+        rimraf(directoryToDelete, function (error) {
             if (error) throw error;
             done();
         });
     });
-
-    describe("Prepare Apache Tomcat with the new release(s) so deploying them later is just a matter of copying the entire folder and starting Tomcat", function () {
-        describe("Create a copy of the backup so you can prepare the new release for later", function () {
-            it("completes without error", function (done) {
-                run.createTomcatBackupFolder(releaseBackupFolderName, function (error) {
+    
+    describe("Take a backup of each servlet container (e.g. Apache Tomcat)", function () {
+        describe("Stop application servers individually", function () {
+            it("Stop tomcat", function () {
+               
+            });
+            it("Check if tomcat has stopped", function () {
+               
+            });
+            it("Force stop tomcat", function () {
+               
+            });
+        });
+        describe("Take backup of all servlet containers state before release (e.g /{tomcat home})", function () {
+            it("sudo mkdir -p /var/tmp/{YYYYMMDDTHH-MM-SS}_tomcatReleaseBackup", function (done) {
+                fileSystem.access(tomcatReleaseBackupUri.replace(timestampPlaceHolder, timestamp), function (error) {
                     if (error) throw error;
-                    run.backupTomcatByServerNumber("1", function (error) {
-                        if (error) throw error;
-                        run.backupTomcatByServerNumber("2", function (error) {
-                            if (error) throw error;
-                            run.createTomcatReleaseFolder(releaseBackupFolderName, function (error) {
-                                if (error) throw error;
-                                run.copyTomcatBackupIntoTomcatReleaseFolder(function (error) {
-                                    if (error) throw error;
-                                    run.createReleaseCandidateFolder(releaseBackupFolderName, function (error) {
-                                        if (error) throw error;
-                                        run.deleteOldApplicationFilesByServerNumberAndApplicationName("app", 1, function (error) {
-                                            if (error) throw error;
-                                            run.deleteOldApplicationFilesByServerNumberAndApplicationName("app.war", 1, function (error) {
-                                                if (error) throw error;
-                                                run.deleteOldApplicationFilesByServerNumberAndApplicationName("app", 2, function (error) {
-                                                    if (error) throw error;
-                                                    run.deleteOldApplicationFilesByServerNumberAndApplicationName("app.war", 2, function (error) {
-                                                        if (error) throw error;
-                                                        done();
-                                                    })
-                                                })
-                                            })  
-                                        })
-                                    });
-                                });
-                            });
-                        });
-                    });
+                    done();
+                });
+            });
+            it("sudo cp -rp /var/tomcat/server{#} /var/tmp/{YYYYMMDDTHH-MM-SS}_tomcatReleaseBackup/", function () {
+               
+            });
+        });
+        describe("Start all servlet containers (e.g. Apache Tomcat)", function () {
+            it("/etc/init.d/tomcat{#} start", function () {
+               
+            });
+            it("/etc/init.d/tomcat{#} status", function () {
+               
+            });
+        });
+    });
+
+    describe("Prepare Apache Tomcat with the new release(s) so deploying them later is just a matter of copying the entire folder and starting Tomcat.", function () {
+        describe("Create a copy of the backup so you can prepare the new release for later", function () {
+            it("sudo mkdir -p /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}", function () {
+               
+            });
+            it("sudo cp -rp /var/tmp/tomcatReleaseBackup_{YYYYMMDDTHH-MM-SS}/* /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/", function () {
+               
+            })
+        });
+        describe("Get ALL release candidates as detailed in release notes and put them onto the server ready for deployment", function () {
+            it("sudo mkdir -p /var/tmp/releaseCandidates_{YYYYMMDDTHH-MM-SS}", function () {
+               
+            });
+            it("cd /var/tmp/releaseCandidates_{YYYYMMDDTHH-MM-SS}", function () {
+               
+            });
+            it("sudo wget --user={nexusUser} --password={nexusPassword} http://{nexusDomain}/{releaseCandidateURI}", function () {
+               
+            });
+        });
+        describe("Delete any old application folders and war files from the servlet container", function () {
+            describe("For example, if you are releasing a new version of map display, then remove its old expanded mom folder", function () {
+                it("sudo rm -rf /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/webapps/{applicationName}", function () {
+                   
+                });
+                it("sudo rm -rf /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/webapps/{applicationName}.war", function () {
+                   
                 });
             });
         });
-
+        describe("Copy ALL new release candidates to the correct servlet container in your 'tomcatRelease' folder.", function () {
+            describe("Refer to 'Additional technical details' table for which Apache tomcat instance to put specific applications into", function () {
+                it("sudo cp -rp /var/tmp/releaseCandidates_{YYYYMMDDTHH-MM-SS}/{releaseCandidate}.war /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/webapps/", function () {
+                   
+                })
+            });
+        });
+        describe("Rename new release candidates to appropriate application name (e.g. mom.war).", function () {
+            describe("Refer to 'Additional technical details' for naming conventions.", function () {
+                it("sudo mv /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/webapps/{releaseCandidate}.war /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/webapps/{applicationName}.war", function () {
+                   
+                });
+            });
+        });
+        describe("Update properties/context files if required as per release notes", function () {
+            describe("Manually update application properties files", function () {
+                it("cd /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/shared/lib", function () {
+                    // update or create properties files here
+                   
+                });
+            });
+            describe("Manually update application context files", function () {
+                it("cd /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/conf", function () {
+                    // update or create context files here
+                   
+                });
+            });
+            describe("Change permissions and ownership of any changed or created properties or context files", function () {
+                it("sudo chmod 644 /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/shared/lib/{applicationName}.properties", function () {
+                   
+                });
+                it("sudo chown tomcat.tomcat /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/shared/lib/{applicationName}.properties", function () {
+                   
+                });
+            });
+            describe("Modify permissions and ownership of new RC war", function () {
+                it("sudo chmod 0664 /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/webapps/{applicationName}.war", function () {
+                   
+                });
+                it("sudo chown tomcat.tomcat /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/webapps/{applicationName}.war", function () {
+                   
+                });
+            });
+            describe("Delete tomcat cached content", function () {
+                it("sudo rm -rf /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/temp/*", function () {
+                   
+                });
+                it("sudo rm -rf /var/tmp/tomcatRelease_{YYYYMMDDTHH-MM-SS}/server{#}/work/Catalina/*", function () {
+                   
+                });
+            });
+        });
     });
 });
